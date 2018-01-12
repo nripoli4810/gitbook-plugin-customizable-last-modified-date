8ef398f5adf17f4eb8993f6fd4657cbb65121d5f
Nate Ripoli <nripoli4810@users.noreply.github.com>
2018-01-11 20:24:50 -0500

    Fixed undefined error when the user has not defined a pluginConfig setting in the book.json file

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ module.exports = {
     hooks: {
         "page:before": function (page) {
             if (page.name != "SUMMARY.md") {
-                var location = this.config.get("pluginConfig").customizableLastModified.location;
+                // get the default location
+                var location = this.config.get('pluginsConfig').customizableLastModified.location;
 
-                if (!(location)) {
-                    location = this.config.get('pluginsConfig').customizableLastModified.location;
+                // check if the user has defined where to place the last modified date
+                var pluginConfig = this.config.get("pluginConfig");
+
+                if( (typeof pluginConfig != "undefined" && pluginConfig !== null) &&
+                    (typeof(pluginConfig.customizableLastModified) != "undefined" && typeof(pluginConfig.customizableLastModified) !== null) &&
+                    (typeof(pluginConfig.customizableLastModified.location) != "undefined" && typeof(pluginConfig.customizableLastModified.location) !== null) ) {
+                    location = this.config.get('pluginConfig').customizableLastModified.location;
                 }
-
-                console.log(location);
 
                 var content = page.content;
 
